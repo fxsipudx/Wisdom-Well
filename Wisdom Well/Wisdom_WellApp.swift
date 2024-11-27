4b5a69183789640f2af32e275e00b65051f984ec
Shubham Jena <146214100+fxsipudx@users.noreply.github.com>
2024-11-27 21:15:52 +0100

    Initial Commit

---
+++ Wisdom Well/Wisdom_WellApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wisdom_WellApp.swift
+//  Wisdom Well
+//
+//  Created by Shubham Jena on 27/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Wisdom_WellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Wisdom WellTests/Wisdom_WellTests.swift
@@ -0,0 +1,17 @@
+//
+//  Wisdom_WellTests.swift
+//  Wisdom WellTests
+//
+//  Created by Shubham Jena on 27/11/24.
+//
+
+import Testing
+@testable import Wisdom_Well
+
+struct Wisdom_WellTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
